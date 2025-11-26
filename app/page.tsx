fb9c01606becb3461e5c6e91403194cf29b2e670
anasfarock <anasf1905@gmail.com>
2025-11-26 19:40:53 +0500

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-blue-500">
+      <h1 className="text-4xl font-bold text-white">Flowo!</h1>
+    </div>
+  )
+}
