8dca964f7135ff79a42954d15aeadb3c8200e17e
anasfarock <anasf1905@gmail.com>
2025-11-26 21:05:52 +0500

    fixed material icons issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,14 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+const materialSymbols = () => {
+  const link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.href =
+    "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200";
+  return link;
+};
+
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
@@ -21,6 +29,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+        />
+      </head>
       <body
         className={`${inter.variable} font-display bg-background-light dark:bg-background-dark text-[#101922] dark:text-background-light antialiased`}
       >
