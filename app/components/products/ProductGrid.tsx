67daff57f94c70233ef196051009fc413694c365
anasfarock <anasf1905@gmail.com>
2025-11-27 14:30:55 +0500

    updated paddings product grid

--- app/components/products/ProductGrid.tsx
+++ app/components/products/ProductGrid.tsx
@@ -36,7 +36,7 @@ export default function ProductGrid({
   }, [products, selectedCategory]);
 
   return (
-    <section className="py-12">
+    <section className="pb-12">
       <div className="container mx-auto px-4">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {filteredProducts.map((product) => (
