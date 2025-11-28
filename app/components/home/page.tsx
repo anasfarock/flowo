661569e553f3e3b85fd7ab9a6594eabd1d69923a
anasfarock <anasf1905@gmail.com>
2025-11-28 12:44:06 +0500

    removed trusted by section from home

--- app/components/home/page.tsx
+++ app/components/home/page.tsx
@@ -11,7 +11,7 @@ export default function HomePage() {
       <Header />
       <main>
         <HeroSection />
-        <TrustedBy />
+        {/* <TrustedBy /> */}
         <FeaturesSection />
         <CTASection />
       </main>
